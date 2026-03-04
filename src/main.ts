2c98a09a94646a8c5fc0fece71ffb91c0d7a4a35
ayala-agronis <ayala@agronis.com>
2026-03-04 10:22:08 +0200

    Remove-logs-in-production

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  const noop = () => {};
+  console.log = noop;
+  console.debug = noop;
+  console.info = noop;
+  console.time = noop;
+  console.timeEnd = noop;
+  console.group = noop;
+  console.groupEnd = noop;
+
+  // console.warn = noop;
+  // console.error = noop;
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
