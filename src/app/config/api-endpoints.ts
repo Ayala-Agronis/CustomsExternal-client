d1173378f73a2c1cea754eb6870442986c2577cf
ayala-agronis <ayala@agronis.com>
2026-03-04 10:21:39 +0200

    Configure-routing-for-production

--- src/app/config/api-endpoints.ts
+++ src/app/config/api-endpoints.ts
@@ -1,5 +1,49 @@
 const isLocalhost = window.location.hostname.includes('localhost');
 
+// לסביבת ייצור
+// export const apiConfig = {
+//   azureBlobsUrl: isLocalhost
+//     ? "http://localhost:84/api/"
+//     // ? "https://localhost:7203/api/"
+//     : "https://azureblobs20250623143905.azurewebsites.net/api/",
+
+//   customsApiUrl: isLocalhost
+//     ? "http://localhost:82/api/"
+//     // ? "http://localhost:63012/api/"
+//     : "https://customsapi20250623140858.azurewebsites.net/api/",
+
+//   customsExternalApiUrl: isLocalhost
+//     ? "http://localhost:81/api/"
+//     // ? "https://localhost:44308/api/"
+//     : "https://customsexternal20250624201845.azurewebsites.net/api/",
+
+//   customsdbApiUrl: isLocalhost
+//     ? "http://localhost:83/api/"
+//     // ? "http://localhost:54748/api/"
+//     : "https://customsdbapi20250623133149.azurewebsites.net/api/",
+//   customsdocSendApiUrl: isLocalhost
+//     ? "http://localhost:85/api/"
+//     // ? "https://localhost:44363/api/"
+//     : "https://customsdbapi20250623133149.azurewebsites.net/api/"
+// };
+
+
+// // מעודכן לסביבת ייצור
+// export const apiConfig = {
+//   azureBlobsUrl:  "/azureblobs/api/",
+
+//   customsApiUrl:  "/customsapi/api/",
+
+//   customsExternalApiUrl:  "/customsexternal/api/",
+
+//   customsdbApiUrl:  "/customsdbapi/api/",
+
+//   customsdocSendApiUrl:  "/customsdocsend/api/"
+// };
+
+
+
+// לסביבת פיתוח
 export const apiConfig = {
   azureBlobsUrl: isLocalhost
     // ? "http://localhost:84/api/"
